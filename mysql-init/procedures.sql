9687dd4144fa7fe365931bdad91b3822e28ef86c
evelynriandrade6 <eveyandrade0.3@gmail.com>
2025-04-07 13:54:48 -0300

    banco de dados

---
+++ mysql-init/procedures.sql
@@ -0,0 +1,65 @@
+delimiter //
+
+create procedure registrar_compra(
+	in p_id_usuario int,
+	in p_id_ingresso int,
+	in pd_id_quantidade int
+)
+begin
+	declare v_id_compra int;
+
+	-- Criar registro na tabela 'compra'
+	insert into compra (data_compra, fk_id_usuario)
+	values (now(), p_id_usuario);
+
+	-- Obter o ID da compra recém-criada
+	set v_id_compra = last_insert_id();
+
+	-- Registrar os ingressos comprados
+	insert into ingresso_compra (fk_id_compra, fk_id_ingresso, quantidade)
+	values (v_id_compra, p_id_ingresso, p_quantidade);
+
+end; //
+delimiter ;
+
+delimiter //
+
+create procedure total_ingresso_usuario(
+
+	in p_id_usuario int,
+	out p_total_ingressos int
+)
+begin
+	-- Inicializar o valor de saída
+	set p_total_ingressos = 0;
+
+	-- Consultar e somar todos os ingressos comprados pelo usuário
+	select coalesce(sum(ic.quantidade), 0)
+	into p_total_ingressos
+	from ingresso_compra ic
+	join compra c on ic.fk_id_compra = c.id_compra
+	where c.fk_id_usuario = p_id_usuario;
+end; //
+
+delimiter ;
+
+show procedure status where db = 'vio_evelyn';
+
+set @total = 0;
+
+call total_ingresso_usuario(2,@total);
+
+delimiter //
+
+create procedure registrar_presenca(
+	in p_id_compra int,
+	in p_id_evento int
+)
+begin
+	-- Registrar presença
+	insert into presenca (data_hora_checkin, fk_id_evento, fk_id_compra)
+	values (now(), p_id_evento, p_id_compra);
+end //
+
+delimiter ;
+
